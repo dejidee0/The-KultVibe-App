02ec78abc64a045104ffffa00aab78437c8b1596
Lazy-Code-X01 <lazycode686@gmail.com>
2026-07-11 00:45:15 +0100

    feat: collapse sidebar to icon-only on /community for more breathing room

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,12 +1,18 @@
+'use client';
+
 import React from 'react';
+import { usePathname } from 'next/navigation';
 import IconBar from './IconBar';
 import ChannelPanel from './ChannelPanel';
 
 export default function Sidebar() {
+  const pathname = usePathname();
+  const iconOnly = pathname === '/community';
+
   return (
     <aside className="flex h-full flex-shrink-0">
       <IconBar />
-      <ChannelPanel />
+      {!iconOnly && <ChannelPanel />}
     </aside>
   );
 }
